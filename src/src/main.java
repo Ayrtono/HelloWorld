8701223a6f61f5bd3db8dd1f1750ff455137b7fe
ayrton <ayrtonobraam@gmail.com>
2017-01-24 16:21:39 +0100

    added some newer lines

--- src/src/main.java
+++ src/src/main.java
@@ -2,7 +2,5 @@
 public class main {
     public static void main(String[] args){
         System.out.println("Hello World!");
-        System.out.println("How are you??????");
-        System.out.println("Why aren't you answering?");
     }
 }
